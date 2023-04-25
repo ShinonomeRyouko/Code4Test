e9e548a74f9bea797a639ba72477b4143c7c2cf5
ShinonomeRyouko <shinonomeryouko@proton.me>
2023-04-25 17:38:33 +0800

    add LeetCode Longest substring without repeat char

---
+++ src/longest_substr_no_repeat_char.cpp
@@ -0,0 +1,27 @@
+#include <unordered_set>
+#include "longest_substr_no_repeat_char.h"
+
+longest_substr_no_repeat_char::longest_substr_no_repeat_char() {
+    
+}
+
+longest_substr_no_repeat_char::~longest_substr_no_repeat_char() {
+    
+}
+
+int longest_substr_no_repeat_char::lengthOfLongestSubstring(string s) {
+    unordered_set<char> filter;
+    int left = 0;
+    int cur = 0;
+    int max_len = 0;
+    for (auto ch : s) {
+        while (filter.find(ch) != filter.end()) {
+            filter.erase(s[left]);
+            left++;
+        }
+        filter.insert(ch);
+        max_len = (cur - left + 1 > max_len) ? cur - left + 1 : max_len;
+        cur++;
+    }
+    return max_len;
+}
---
+++ test/longest_substr_no_repeat_char_test.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include "longest_substr_no_repeat_char.h"
+
+
+TEST(longest_substr_no_repeat_charTest, SimpleTest) {
+    longest_substr_no_repeat_char* obj = new longest_substr_no_repeat_char();
+    ASSERT_EQ(obj->lengthOfLongestSubstring("abcabcbb"), 3);
+    ASSERT_EQ(obj->lengthOfLongestSubstring("bbbbb"), 1);
+    ASSERT_EQ(obj->lengthOfLongestSubstring("pwwkew"), 3);
+    delete obj;
+}
