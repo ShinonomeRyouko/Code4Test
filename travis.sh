9f4a203d9ba1606a1ac8134e6d183e06be957ca3
ShinonomeRyouko <shinonomeryouko@proton.me>
2023-04-29 23:39:43 +0800

    fix: use clang-format instead of clang-format-14 in travis ci

--- travis.sh
+++ travis.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-find -name "*.cpp" -o -name "*.h" | xargs clang-format-14 --dry-run --Werror
+find -name "*.cpp" -o -name "*.h" | xargs clang-format --dry-run --Werror
 cppcheck -ibuild . --force --quiet --error-exitcode=1 --enable=warning,performance,portability --suppress=missingIncludeSystem
 mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Debug -DSANITIZER_CHECK=ON -DCODE_COVERAGE=ON .. && make -j $(nproc)
