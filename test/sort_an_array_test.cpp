51eaef6a4f96de9a80628e32ec40f4e23524f07f
ShinonomeRyouko <shinonomeryouko@proton.me>
2023-04-30 23:55:12 +0800

    feat(*): LeetCode sort algorithm heap sort and merge sort

--- test/sort_an_array_test.cpp
+++ test/sort_an_array_test.cpp
@@ -13,4 +13,12 @@ TEST(sort_an_arrayTest, SimpleTest) {
     // if too many equal val.
     vector<int> conner_case{2, 2, 2, 2, 2, 2, 2, 2, 2};
     QuickSort(arr);
+
+    arr = {9, 8, 7, 7, 7, 6, 5, 5};
+    HeapSort(arr);
+    EXPECT_TRUE(is_sorted(arr.begin(), arr.end()));
+
+    arr = {9, 8, 7, 7, 7, 6, 5, 5};
+    MergeSort(arr);
+    EXPECT_TRUE(is_sorted(arr.begin(), arr.end()));
 }
